63f90b6a68814da3a76e9e49f4931aa8746eedd2
Shreya Kumari <138003477+shreya1m@users.noreply.github.com>
2024-02-21 13:51:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -105,22 +105,7 @@ app.get("/posts/:postId", (req, res) => {
 });
 
   
-app.listen(3006, function() {
-  console.log("Server started on port 3006");
+app.listen(3000, () =>{
+  console.log("Server started on port 3000");
 });
 
-
-
-
-// app.get("/posts/:postId",function(req,res)
-// {
-//     const requestedPostId=(req.params.postId);//converting to lowercase using lodash 
-    
-//     posts.findOne({_id: requestedPostId}, function(err, post){
-//         res.render("post", {
-//           title: post.title,
-//           content: post.content
-//         });
-//       });
-    
-//     });
